46e47502b727817070d309ee2fa67ea833b227c2
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-27 13:53:47 -0300

    arquivo para colocar minhas macros

---
+++ macro_utils.h
@@ -0,0 +1,9 @@
+#ifndef MACRO_UTILS_H
+#define MACRO_UTILS_H
+
+/**
+ * @brief uma abreviação para o padrão de iteração que costumo usar
+ */
+#define FOR_EACH(expression) for (auto &it : expression)
+
+#endif // MACRO_UTILS_H
